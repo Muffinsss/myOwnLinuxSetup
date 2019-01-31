f1b8e1d142a8fb012e638174b8c679b9f406bfd1
muffins <zyctxbox@gmail.com>
2019-01-31 01:49:48 -0600

    Update linuxSetup.sh

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -1,10 +1,14 @@
 #Update Debian Operating System
 apt-get update -q -y
+
+#install dbus
+dpkg --configure -a
+apt-get install dbus
+
+#Upgrade again
 apt-get upgrade -q -y
 apt-get dist-upgrade -q -y
 
-#install dbus
-apt-get install dbus
 
 # Install X & LXDE & VNC
 apt-get -q -y install xorg lxde-core tightvncserver
