d740e472d649c4f2fa69689c36ebaf84de221bf8
muffins <zyctxbox@gmail.com>
2019-01-31 02:08:33 -0600

    Update linuxSetup.sh

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -1,9 +1,8 @@
 #Update Debian Operating System
-apt-get update -q -y
-
+apt-get -q -y update
 #install dbus
+apt-get -q -y install dbus
 dpkg --configure -a
-apt-get install dbus
 
 #Upgrade again
 apt-get upgrade -q -y
@@ -16,14 +15,14 @@ apt-get -q -y install xorg lxde-core tightvncserver
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 
 #Install Java 8 
-apt-get install software-properties-common
+apt-get -q -y install software-properties-common
 add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
-sudo apt-get update 2>&1 1>/dev/null | sed -ne 's/.*NO_PUBKEY //p' |
+sudo apt-get -q -y update 2>&1 1>/dev/null | sed -ne 's/.*NO_PUBKEY //p' |
 while read key; do
     echo 'Processing key:' "$key"
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "$key"
 done
-apt-get install -q -y oracle-java8-installer
+apt-get -q -y install -q -y oracle-java8-installer
 
 #Dreambot Stuff
 mkdir ~/Desktop/
@@ -31,7 +30,7 @@ curl -o ~/Desktop/DBLauncher.jar https://dreambot.org/DBlauncher.jar
 chmod 777 ~/Desktop/DBLauncher.jar
 
 #Install Chromium
-apt-get install chromium
+apt-get -q -y install chromium
 
 #Asks for pw here
 tightvncserver :1
