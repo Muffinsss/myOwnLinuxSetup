fa7f9c2e4c4598b75496eb4f34fade7ba8570c58
muffins <zyctxbox@gmail.com>
2019-01-31 01:23:12 -0600

    Linux Repo.sh
    try

---
+++ linuxSetup.sh
@@ -0,0 +1,43 @@
+#Update Debian Operating System
+
+apt-get update -q -y
+apt-get upgrade -q -y
+apt-get dist-upgrade -q -y
+
+# Install X & LXDE & VNC
+apt-get -q -y install xorg lxde-core tightvncserver
+
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+
+#Install Java 8 
+
+apt-get install software-properties-common
+add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
+sudo apt-get update 2>&1 1>/dev/null | sed -ne 's/.*NO_PUBKEY //p' |
+while read key; do
+    echo 'Processing key:' "$key"
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "$key"
+done
+apt-get install -q -y oracle-java8-installer
+
+#Dreambot Stuff
+
+mkdir ~/Desktop/
+curl -o ~/Desktop/DBLauncher.jar https://dreambot.org/DBlauncher.jar
+chmod +x ~/Desktop/DBLauncher.jar
+
+#Install Chromium
+#install chromium
+apt-get install chromium
+
+#Asks for pw here
+tightvncserver :1
+tightvncserver -kill :1
+
+
+#add configs
+wget https://gist.github.com/anonymous/d0606a74a17247134e74b6f20abe24ab/raw/7be591567fc5e3324fcac5d7edfc63d36a3684b4/xstartup -O ~/.vnc/xstartup
+
+#boot VNC Server
+tightvncserver :1
+
